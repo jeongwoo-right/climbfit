10d8d3fbb06defe29dcdc7032c0860e1eb7ae6a3
jeongwoo-right <jeongwoocode@gmail.com>
2025-06-20 11:07:45 +0900

    refactor: update PlanAdapter  structure �

--- android_project/app/src/main/java/com/scsa/myproject/PlanAdapter.java
+++ android_project/app/src/main/java/com/scsa/myproject/PlanAdapter.java
@@ -52,8 +52,8 @@ public class PlanAdapter extends ArrayAdapter<ClimbingPlan> {
 
         ClimbingPlan plan = planList.get(position);
         holder.textInfo.setText(
-                plan.getDate() + " | " + plan.getPlace() + " - Goal: " + plan.getGoal()
-                        + (plan.getRecord().isEmpty() ? "" : "\nRecord: " + plan.getRecord())
+                plan.getDate() + " | " + plan.getPlace() + "\n" +
+                        "Goal: " + plan.getGoal() + (plan.getRecord().isEmpty() ? "" : " | Record: " + plan.getRecord())
         );
 
         if (isPendingList) {
